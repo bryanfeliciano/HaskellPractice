a3966df9f66969d30de15e943ae2000f682f81f4
bryan <bryanfeliciano892@gmail.com>
2022-03-05 08:41:40 -0600

    commit on 'main'

--- leetCode.hs
+++ leetCode.hs
@@ -5,3 +5,9 @@ import qualified Data.Set as S
 twoSum::[Int]->Int->Bool
 twoSum xs target = any(\x->target-x /=x && S.member(target-x)mySet)xs
     where mySet = S.fromList xs
+
+--find the way to filter all odd and even elem from a list
+findOutlier::[Int]->Int
+findOutlier xs
+         |[n]<-filter even xs = n
+         |[n]<-filter odd  xs = n
