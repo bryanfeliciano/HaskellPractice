f29f9eedaa6b485be99c91eed866e165026cc137
bryan <bryanfeliciano892@gmail.com>
2022-03-18 19:25:20 -0500

    commit on ' main'

---
+++ haskellPracticeBasics.hs
@@ -0,0 +1,16 @@
+elem' :: (Eq a)=> a->[a]->Bool
+elem' _ [] = False
+elem' x (y:ys) = if x == y then True else elem' x ys
+
+nub' :: (Eq a) => [a]->[a]
+nub' [] = []
+nub' (x:xs)
+     |x `elem` xs = nub' xs
+     |otherwise = x:nub' xs
+
+isAsc :: [Int]->Bool
+isAsc [] = True 
+isAsc [x] = True 
+isAsc (x:y:xs)
+   |(x<y) = False 
+   |otherwise = isAsc xs
